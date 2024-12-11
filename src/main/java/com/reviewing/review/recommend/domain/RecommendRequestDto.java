1ee92dd43d6a6ad0d9aff080ee2d971e23da786f
seoyeoning <minalunpark@naver.com>
2024-12-11 16:56:41 +0900

    chore: request 수정

--- src/main/java/com/reviewing/review/recommend/domain/RecommendRequestDto.java
+++ src/main/java/com/reviewing/review/recommend/domain/RecommendRequestDto.java
@@ -1,8 +1,12 @@
 package com.reviewing.review.recommend.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class RecommendRequestDto {
 
     private String question;
