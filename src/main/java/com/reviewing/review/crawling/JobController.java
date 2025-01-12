12f4af1b0b71ca5be47375576f61fc77eef91137
seoyeoning <minalunpark@naver.com>
2025-01-12 13:15:56 +0900

    feat: 4개 Job 동시 실행 기능 추가

--- src/main/java/com/reviewing/review/crawling/JobController.java
+++ src/main/java/com/reviewing/review/crawling/JobController.java
@@ -1,6 +1,7 @@
 package com.reviewing.review.crawling;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.configuration.JobRegistry;
@@ -11,13 +12,43 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class JobController {
 
     private final JobLauncher jobLauncher;
     private final JobRegistry jobRegistry;
 
-    @GetMapping("/first")
-    public String firstApi(@RequestParam("value") String value) throws Exception {
+    @GetMapping("/allCrawling")
+    public String runAllJobs(@RequestParam("value") String value) throws Exception {
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addString("date", value)
+                .toJobParameters();
+
+        try {
+            jobLauncher.run(jobRegistry.getJob("nomadcodersCrawlingJob"), jobParameters);
+        } catch (Exception e) {
+            log.error("Job 살패", e);
+        }
+        try {
+            jobLauncher.run(jobRegistry.getJob("codeitCrawlingJob"), jobParameters);
+        } catch (Exception e) {
+            log.error("Job 살패", e);
+        }
+        try {
+            jobLauncher.run(jobRegistry.getJob("fastcampusCrawlingJob"), jobParameters);
+        } catch (Exception e) {
+            log.error("Job 살패", e);
+        }
+        try {
+            jobLauncher.run(jobRegistry.getJob("inflearnCrawlingJob"), jobParameters);
+        } catch (Exception e) {
+            log.error("Job 살패", e);
+        }
+        return "ok";
+    }
+
+    @GetMapping("/nomadcodersCrawling")
+    public String nomadcodersCrawling(@RequestParam("value") String value) throws Exception {
         JobParameters jobParameters = new JobParametersBuilder()
                 .addString("date", value)
                 .toJobParameters();
@@ -25,35 +56,29 @@ public class JobController {
         return "ok";
     }
 
-    @GetMapping("/second")
-    public String secondApi(@RequestParam("value") String value) throws Exception {
-
+    @GetMapping("/codeitCrawling")
+    public String codeitCrawling(@RequestParam("value") String value) throws Exception {
         JobParameters jobParameters = new JobParametersBuilder()
                 .addString("date", value)
                 .toJobParameters();
-
         jobLauncher.run(jobRegistry.getJob("codeitCrawlingJob"), jobParameters);
         return "ok";
     }
 
-    @GetMapping("/third")
-    public String thirdApi(@RequestParam("value") String value) throws Exception {
-
+    @GetMapping("/fastcampusCrawling")
+    public String fastcampusCrawling(@RequestParam("value") String value) throws Exception {
         JobParameters jobParameters = new JobParametersBuilder()
                 .addString("date", value)
                 .toJobParameters();
-
         jobLauncher.run(jobRegistry.getJob("fastcampusCrawlingJob"), jobParameters);
         return "ok";
     }
 
-    @GetMapping("/fourth")
-    public String fourthApi(@RequestParam("value") String value) throws Exception {
-
+    @GetMapping("/inflearnCrawling")
+    public String inflearnCrawling(@RequestParam("value") String value) throws Exception {
         JobParameters jobParameters = new JobParametersBuilder()
                 .addString("date", value)
                 .toJobParameters();
-
         jobLauncher.run(jobRegistry.getJob("inflearnCrawlingJob"), jobParameters);
         return "ok";
     }
