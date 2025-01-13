27cc873f83a5356a8bd24d84832f7b636a2fd97f
seoyeoning <minalunpark@naver.com>
2025-01-13 17:36:55 +0900

    chore: 관리자 리뷰 조회 res에 날짜 추가, 서브모듈 local db 2개 추가

--- src/main/java/com/reviewing/review/admin/domain/AdminReviewResponseDto.java
+++ src/main/java/com/reviewing/review/admin/domain/AdminReviewResponseDto.java
@@ -1,6 +1,9 @@
 package com.reviewing.review.admin.domain;
 
 import com.reviewing.review.review.domain.ReviewStateType;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -20,5 +23,24 @@ public class AdminReviewResponseDto {
     private String reviewContents;
     private ReviewStateType reviewStatus;
     private String reviewCertification;
+    private String updatedAt;
 
+    public AdminReviewResponseDto(UUID courseId, String courseTitle, String courseTeacher,
+            String courseThumbnailImage, String courseThumbnailVideo, String courseUrl,
+            Long reviewId,
+            String reviewContents, ReviewStateType reviewStatus, String reviewCertification,
+            LocalDateTime updatedAt) {
+        this.courseId = courseId;
+        this.courseTitle = courseTitle;
+        this.courseTeacher = courseTeacher;
+        this.courseThumbnailImage = courseThumbnailImage;
+        this.courseThumbnailVideo = courseThumbnailVideo;
+        this.courseUrl = courseUrl;
+        this.reviewId = reviewId;
+        this.reviewContents = reviewContents;
+        this.reviewStatus = reviewStatus;
+        this.reviewCertification = reviewCertification;
+        this.updatedAt = updatedAt.format(
+                DateTimeFormatter.ofPattern("yyyy.MM.dd hh.mm a", Locale.US));
+    }
 }
