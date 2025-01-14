11c26eea980d14b7036e254a9ba8ca13274e3b22
seoyeoning <minalunpark@naver.com>
2025-01-14 12:50:36 +0900

    노마드 element 내용 바뀜

--- src/main/java/com/reviewing/review/crawling/batch/nomadcoders/NomadcodersReader.java
+++ src/main/java/com/reviewing/review/crawling/batch/nomadcoders/NomadcodersReader.java
@@ -88,7 +88,7 @@ public class NomadcodersReader implements ItemStreamReader<Course> {
         }
         log.info("무한 스크롤 나옴");
         List<WebElement> courses = driver.findElements(By.cssSelector(
-                "div.sc-4a1d66b0-0.cZeVxA.flex.flex-col.relative.rounded-lg.items-center"));
+                "div.sc-4a1d66b0-0.jiwdzH.flex.flex-col.relative.rounded-lg.items-center"));
 
 
         for (WebElement course : courses) {
